41542b8d8ac4a74bd1018f3fe7157e05b78bf045
Kishore Mohanavelu <kishoremohanavelu@Kishores-MacBook-Pro.local>
2020-05-26 11:08:27 +0530

    testing env variable

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,23 +1,14 @@
 package org.example;
 
 import org.testng.annotations.Test;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Properties;
-
 import static org.testng.AssertJUnit.assertTrue;
 
 
 public class AppTest {
     @Test
     public void shouldAnswerWithTrue() throws Exception {
-        Properties prop = new Properties();
-        prop.load(new FileReader(new File("output.properties")));
-        prop.setProperty("android","10");
-        prop.setProperty("ios","11");
-        prop.setProperty("xcode","12");
+        System.out.println(System.getProperty("IOS_VERSION"));
+        System.setProperty("IOS_VERSION","12");
         assertTrue( true );
 
     }
