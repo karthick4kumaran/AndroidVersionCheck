dda6aefa10814f6722cd84e5ea44c1744f05bc1e
Kishore Mohanavelu <kishoremohanavelu@Kishores-MacBook-Pro.local>
2020-05-26 10:57:14 +0530

    properties reading added

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,15 +2,22 @@ package org.example;
 
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Properties;
+
 import static org.testng.AssertJUnit.assertTrue;
 
 
 public class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        System.setProperty("AndroidVersion","NewAndroidVersionReleased");
-        System.setProperty("IOSVersion","NewIOSVersionReleased");
+    public void shouldAnswerWithTrue() throws Exception {
+        Properties prop = new Properties();
+        prop.load(new FileReader(new File("output.properties")));
+        prop.setProperty("android","10");
+        prop.setProperty("ios","11");
+        prop.setProperty("xcode","12");
         assertTrue( true );
 
     }
