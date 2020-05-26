86199216868849027cdeea11c96d03230b9f8d47
Kishore Mohanavelu <kishoremohanavelu@Kishores-MacBook-Pro.local>
2020-05-26 11:14:21 +0530

    modified the properties

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -7,8 +7,9 @@ import static org.testng.AssertJUnit.assertTrue;
 public class AppTest {
     @Test
     public void shouldAnswerWithTrue() throws Exception {
-        System.out.println(System.getProperty("IOS_VERSION"));
-        System.setProperty("IOS_VERSION","12");
+        System.out.println("Old Version of Android is "+System.getProperty("android"));
+        System.setProperty("android","11");
+        System.out.println("New Version of Android is "+System.getProperty("android"));
         assertTrue( true );
 
     }
