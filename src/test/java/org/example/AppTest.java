86bb3137e4d7f3f0f715e87d10b43b809a56c62e
Kishore Mohanavelu <kishoremohanavelu@Kishores-MacBook-Pro.local>
2020-05-26 12:06:05 +0530

    updated code

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.PrintWriter;
 import java.util.Properties;
 
 import static org.testng.AssertJUnit.assertTrue;
@@ -12,12 +13,9 @@ import static org.testng.AssertJUnit.assertTrue;
 public class AppTest {
     @Test
     public void shouldAnswerWithTrue() throws Exception {
-        Properties prop = new Properties();
-        prop.load(new FileReader(new File("output.properties")));
-        prop.setProperty("android","10");
-        prop.setProperty("ios","11");
-        prop.setProperty("xcode","12");
-        assertTrue( true );
+        PrintWriter pw = new PrintWriter("updatedVersion.txt");
+        pw.write("12");
+        pw.close();
 
     }
 }
